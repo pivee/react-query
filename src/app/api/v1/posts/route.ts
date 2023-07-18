f2a3efa255cb4c866d9eb873803727d770efc944
Pivithuru Jayakody <pivithurujayakody@gmail.com>
2023-07-18 18:28:11 +0530

    Create DELETE route

--- src/app/api/v1/posts/route.ts
+++ src/app/api/v1/posts/route.ts
@@ -1,8 +1,20 @@
 import { prisma } from '@/libs/prisma';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET() {
   const posts = await prisma.post.findMany();
 
   return NextResponse.json(posts);
+}
+
+export async function DELETE(request: NextRequest) {
+  const body = await request.json();
+
+  const post = await prisma.post.delete({
+    where: {
+      id: body.id
+    }
+  });
+
+  return NextResponse.json(post);
 }
